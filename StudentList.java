c676d981b89d8c44e37708e117c2a853a2888a7e
RAISUL <raisulislam109257@gmail.com>
2024-10-04 20:57:07 +0600

    constant decleare step-5

--- StudentList.java
+++ StudentList.java
@@ -14,7 +14,7 @@ public class StudentList {
                 try {
                     BufferedReader fRead = new BufferedReader(
                             new InputStreamReader(
-                                    new FileInputStream("students.txt")));
+                                    new FileInputStream(Constants.fileName)));
                     String fLine = fRead.readLine();
                     String names[] = fLine.split(",");
                     for (String name : names) {
@@ -28,7 +28,7 @@ public class StudentList {
                 try {
                     BufferedReader fRead = new BufferedReader(
                             new InputStreamReader(
-                                    new FileInputStream("students.txt")));
+                                    new FileInputStream(Constants.fileName)));
                     String fLine = fRead.readLine();
                     System.out.println(fLine);
                     String i[] = fLine.split(",");
@@ -42,7 +42,7 @@ public class StudentList {
                 System.out.println("Loading data ...");
                 try {
                     BufferedWriter s = new BufferedWriter(
-                            new FileWriter("students.txt", true));
+                            new FileWriter(Constants.fileName, true));
                     String t = args[0].substring(1);
                     Date d = new Date();
                     String df = "dd/mm/yyyy-hh:mm:ss a";
@@ -59,7 +59,7 @@ public class StudentList {
                 try {
                     BufferedReader s = new BufferedReader(
                             new InputStreamReader(
-                                    new FileInputStream("students.txt")));
+                                    new FileInputStream(Constants.fileName)));
                     String r = s.readLine();
                     String i[] = r.split(",");
                     boolean done = false;
@@ -78,7 +78,7 @@ public class StudentList {
                 try {
                     BufferedReader s = new BufferedReader(
                             new InputStreamReader(
-                                    new FileInputStream("students.txt")));
+                                    new FileInputStream(Constants.fileName)));
                     String D = s.readLine();
                     char a[] = D.toCharArray();
                     boolean in_word = false;
